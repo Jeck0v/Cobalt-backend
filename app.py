c142ee50fa7c8a8141bd0f45e21315fec580e986
Jeck0v <arnaud.fischer75@gmail.com>
2025-02-12 14:44:39 +0100

    :seedling: init project

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, request, make_response
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from pymango import MongoClient
 
 app = Flask(__name__)
+
+# tout rajouter dans un .env
+server = "mongodb://localhost:27017/"
+username = "cobaltDB"
+password = ""
+port = 27017
+
+if server == "localhost":
+    client = MongoClient("mongodb://mongo", username=username, password=password, port=port)
+
+else:
+    client = MongoClient(server, username=username, password=password, port=port)
+
+# faire le docker-compose etc...
+
 app.config['JWT_SECRET_KEY'] = 'web2'
 jwt = JWTManager(app)
 
@@ -29,30 +45,38 @@ def connexion():
 def register():
     firstname = request.form['firstname']
     name = request.form['name']
-    username = request.form['username']
     password = request.form['password']
     email = request.form['email']
 
-    user = {"id": len(users) + 1, "firstname": firstname, "name": name, "username": username, "password": password, "email": email}
+    user = {"id": len(users) + 1, "firstname": firstname, "name": name, "password": password, "email": email}
     users.append(user)
 
     return "User added successfully" + str(users)
 
-@app.route('/signup')
+@app.route('/signin')
 def signin():
-   return "hello"
+   return render_template('signin.html')
 
 
-@app.route('/login')
+@app.route('/')
 def index():
-    return "hello"
+    return render_template('login.html')
 
 
 @app.route('/app')
 @jwt_required()
 def app():
-    return "hello"
+    return render_template('app.html')
 
+@app.route('/app/produit',methods=['GET'])
+@jwt_required()
+def produit():
+    return "prduit = img + titre + prix + descr + quantité"
+
+@app.route('/app/quantite',methods=['GET'])
+@jwt_required()
+def quantite():
+    return "on verif la quantité, si elle est supérieur à 0 alors on accepte la commande sinon, on préviens l'user qu'il n'y a pas de quantité"
 
 
 if __name__ == '__main__':
