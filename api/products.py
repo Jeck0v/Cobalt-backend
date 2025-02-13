cbd7bbc299f7f9267e4ebcd7d3dc5ad842b28cea
Jeck0v <arnaud.fischer75@gmail.com>
2025-02-13 01:20:52 +0100

    :seedling: add products routes + swagger ui

--- api/products.py
+++ api/products.py
@@ -1,14 +1,101 @@
-from flask_restx import Namespace, Resource
+from flask import request
+from flask_restx import Namespace, Resource, fields
 from db.database import db
 
+# namespace routes products
 products_ns = Namespace('products', description='Opérations liées aux produits')
+
+# Model Product Swagger
+product_model = products_ns.model('Product', {
+    'id': fields.String(required=True, description='ID du produit'),
+    'titre': fields.String(required=True, description='Titre du produit'),
+    'description': fields.String(required=True, description='Description du produit'),
+    'prix': fields.Float(required=True, description='Prix du produit'),
+    'quantite': fields.Integer(required=True, description='Quantité disponible en stock'),
+    'image_url': fields.String(description='URL de l\'image du produit')
+})
 
 @products_ns.route('/')
 class Products(Resource):
+    @products_ns.marshal_list_with(product_model)
     def get(self):
-        """Récupérer tous les produits"""
+        """
+        Récupérer tous les produits
+        """
         products = list(db.products.find({}))
-        return {"products": products}, 200
+        return products, 200
+
+    @products_ns.expect(product_model)
+    @products_ns.response(201, 'Produit ajouté')
+    def post(self):
+        """
+        Ajouter un nouveau produit
+        """
+        data = request.get_json()
+        db.products.insert_one(data)
+        return {"msg": "Product added successfully"}, 201
+
+@products_ns.route('/seed')
+class SeedProducts(Resource):
+    def post(self):
+        """
+        Remplir la collection products avec des produits fictifs
+        """
+        products = [
+            {
+                "id": "1",
+                "titre": "Bracelet en cuir",
+                "description": "Bracelet en cuir véritable pour homme.",
+                "prix": 29.99,
+                "quantite": 50,
+                "image_url": "https://www.lapetitecannoise.com/333-large_default/bracelet-homme-cuir-argent.jpg"
+            },
+            {
+                "id": "2",
+                "titre": "Montre élégante et rétro",
+                "description": "Montre élégante, et rétro en cuir pour homme. ",
+                "prix": 199.99,
+                "quantite": 12,
+                "image_url": "https://maison-fevre.fr/cdn/shop/products/bracelet-montre-retro-cuir-marron-homme-maison-fevre-fait-main_900x.jpg?v=1725021036"
+            },
+            {
+                "id": "3",
+                "titre": "Bague en argent",
+                "description": "Bague en argent 925 pour homme.",
+                "prix": 49.99,
+                "quantite": 20,
+                "image_url": "https://itsara.net/cdn/shop/products/bague-homme-en-argent-ethnique-n027p1_Itsara_bijoux_2048x.jpg?v=1737535078"
+            },
+            {
+                "id": "4",
+                "titre": "Collier en acier",
+                "description": "Collier en acier inoxydable pour homme.",
+                "prix": 49.99,
+                "quantite": 40,
+                "image_url": "https://www.ninanina.fr/wp-content/uploads/2019/12/n3575-chaine-figaro-homme-acier-inoxydable-argent-50-cm-bijoux-colliers-hommes-en-ligne-ninanina.jpg"
+            }
+        ]
+
+        db.products.insert_many(products)
+        return {"msg": "Products added successfully"}, 201
+
+@products_ns.route('/<string:product_id>')
+class ProductDetail(Resource):
+    @products_ns.marshal_with(product_model)
+    @products_ns.response(200, 'Produit trouvé')
+    @products_ns.response(404, 'Produit non trouvé')
+    def get(self, product_id):
+        """
+        recup produit par ID
+        """
+        product = db.products.find_one({"id": product_id})
+        if product:
+            return product, 200
+        else:
+            return {"msg": "Product not found"}, 404
 
 def init_products_routes(api):
+    """
+    init routes API
+    """
     api.add_namespace(products_ns)
