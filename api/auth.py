e6364c6e81914d5d711237eac691cf9d8cc973e6
AkaTFL <coldway3108@gmail.com>
2025-02-13 14:37:47 +0100

    fix + ajout

--- api/auth.py
+++ api/auth.py
@@ -1,6 +1,6 @@
 from flask import request
 from flask_restx import Namespace, Resource, fields
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from db.database import db
 from models.user import User
 
@@ -9,6 +9,7 @@ auth_ns = Namespace('auth', description='Opérations liées à l\'authentificati
 
 # model User pour Swagger
 user_model = auth_ns.model('User', {
+    '_id': fields.String(description='ID unique de l\'utilisateur'),
     'firstname': fields.String(required=True, description='Prénom de l\'utilisateur'),
     'name': fields.String(required=True, description='Nom de l\'utilisateur'),
     'password': fields.String(required=True, description='Mot de passe de l\'utilisateur'),
@@ -67,6 +68,49 @@ class Login(Resource):
             return {"access_token": access_token}, 200
         else:
             return {"msg": "Bad username or password"}, 401
+
+@auth_ns.route('/modify')
+class Modify(Resource):
+    @jwt_required()
+    @auth_ns.expect(user_model)
+    @auth_ns.response(200, 'Utilisateur modifié')
+    @auth_ns.response(401, 'Utilisateur non connecté')
+    def put(self):
+        """
+        Modifier les informations de l'utilisateur connecté
+        """
+        Aemail = get_jwt_identity()
+        
+
+        data = request.get_json()
+        firstname = data.get('firstname')
+        name = data.get('name')
+        password = data.get('password')
+        email = data.get('email')
+
+        user = db.users.find_one({"email": Aemail})
+        if user:
+            db.users.update_one({"email": Aemail}, {"$set": {"firstname": firstname, "name": name, "password": password, "email": email}})
+            return {"msg": "User modified"}, 200
+        else:
+            return {"msg": "No user connected"}, 401
+
+@auth_ns.route('/view')
+class View(Resource):
+    @jwt_required()
+    @auth_ns.marshal_with(user_model)
+    def get(self):
+        """
+        Récupérer les informations de l'utilisateur connecté
+        """
+        email = get_jwt_identity()
+        user = db.users.find_one({"email": email})
+        if user:
+            user["_id"] = str(user["_id"])
+            # Ne pas renvoyer le mot de passe dans la réponse
+            return user, 200
+        else:
+            return {"msg": "No user connected"}, 401
 
 def init_auth_routes(api):
     """
