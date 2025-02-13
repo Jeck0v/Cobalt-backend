9e2b5869507dd2b313930a7847f61673e3647ad6
Jeck0v <arnaud.fischer75@gmail.com>
2025-02-13 11:44:31 +0100

    :boom: ça casse ou ça passe

--- db/database.py
+++ db/database.py
@@ -8,3 +8,22 @@ client = MongoClient(
 )
 
 db = client.cobalt_db
+
+
+def initialize_database():
+    """
+    Initialise la base de données en créant les collections nécessaires si elles n'existent pas déjà.
+    """
+    # Liste des collections nécessaires
+    required_collections = ['users', 'products', 'orders']
+    existing_collections = db.list_collection_names()
+    for collection in required_collections:
+        if collection not in existing_collections:
+            db.create_collection(collection)
+            print(f"Collection '{collection}' créée.")
+        else:
+            print(f"Collection '{collection}' existe déjà.")
+
+
+# Appeler la fonction d'initialisation lors de l'importation du module
+initialize_database()
