0ab25eff5c8c493eccfba9dfd828687b005ed028
Jeck0v <arnaud.fischer75@gmail.com>
2025-02-13 14:29:16 +0100

    add category

--- api/products.py
+++ api/products.py
@@ -10,6 +10,7 @@ product_model = products_ns.model('Product', {
     'id': fields.String(required=True, description='ID du produit'),
     'titre': fields.String(required=True, description='Titre du produit'),
     'description': fields.String(required=True, description='Description du produit'),
+    'categorie': fields.String(required=True, description='categorie du produit'),
     'prix': fields.Float(required=True, description='Prix du produit'),
     'quantite': fields.Integer(required=True, description='Quantité disponible en stock'),
     'image_url': fields.String(description='URL de l\'image du produit')
@@ -44,35 +45,57 @@ class SeedProducts(Resource):
         products = [
             {
                 "id": "1",
-                "titre": "Bracelet en cuir",
-                "description": "Bracelet en cuir véritable pour homme.",
+                "titre": "Leather Bracelet",
+                "description": "Genuine leather bracelet for men.",
                 "prix": 29.99,
                 "quantite": 50,
-                "image_url": "https://www.lapetitecannoise.com/333-large_default/bracelet-homme-cuir-argent.jpg"
+                "image_url": "https://www.lapetitecannoise.com/333-large_default/bracelet-homme-cuir-argent.jpg",
+                "categorie": "Bracelets"
             },
             {
                 "id": "2",
-                "titre": "Montre élégante et rétro",
-                "description": "Montre élégante, et rétro en cuir pour homme. ",
+                "titre": "Elegant and Retro Watch",
+                "description": "Elegant and retro leather watch for men.",
                 "prix": 199.99,
                 "quantite": 12,
-                "image_url": "https://maison-fevre.fr/cdn/shop/products/bracelet-montre-retro-cuir-marron-homme-maison-fevre-fait-main_900x.jpg?v=1725021036"
+                "image_url": "https://maison-fevre.fr/cdn/shop/products/bracelet-montre-retro-cuir-marron-homme-maison-fevre-fait-main_900x.jpg?v=1725021036",
+                "categorie": "Watches"
             },
             {
                 "id": "3",
-                "titre": "Bague en argent",
-                "description": "Bague en argent 925 pour homme.",
+                "titre": "Silver Ring",
+                "description": "925 silver ring for men.",
                 "prix": 49.99,
                 "quantite": 20,
-                "image_url": "https://itsara.net/cdn/shop/products/bague-homme-en-argent-ethnique-n027p1_Itsara_bijoux_2048x.jpg?v=1737535078"
+                "image_url": "https://itsara.net/cdn/shop/products/bague-homme-en-argent-ethnique-n027p1_Itsara_bijoux_2048x.jpg?v=1737535078",
+                "categorie": "Rings"
             },
             {
                 "id": "4",
-                "titre": "Collier en acier",
-                "description": "Collier en acier inoxydable pour homme.",
+                "titre": "Stainless Steel Necklace",
+                "description": "Stainless steel necklace for men.",
                 "prix": 49.99,
                 "quantite": 40,
-                "image_url": "https://www.ninanina.fr/wp-content/uploads/2019/12/n3575-chaine-figaro-homme-acier-inoxydable-argent-50-cm-bijoux-colliers-hommes-en-ligne-ninanina.jpg"
+                "image_url": "https://www.ninanina.fr/wp-content/uploads/2019/12/n3575-chaine-figaro-homme-acier-inoxydable-argent-50-cm-bijoux-colliers-hommes-en-ligne-ninanina.jpg",
+                "categorie": "Necklaces"
+            },
+            {
+                "id": "5",
+                "titre": "Cobalt Pendant",
+                "description": "Elegant Cobalt pendant for men.",
+                "prix": 299.99,
+                "quantite": 15,
+                "image_url": "https://caratsutra.in/cdn/shop/products/IMG_20220216_213558.jpg?v=1654097796",
+                "categorie": "Pendants"
+            },
+            {
+                "id": "6",
+                "titre": "Gold Hoop Earrings",
+                "description": "Stylish gold hoop earrings for men.",
+                "prix": 179.99,
+                "quantite": 30,
+                "image_url": "https://www.gld.com/cdn/shop/products/micro-double-row-huggie-earrings-in-yellow-gold-gld-men-3_375x375_crop_center@2x.jpg?v=1680691085%20x2,%20//www.gld.com/cdn/shop/products/micro-double-row-huggie-earrings-in-yellow-gold-gld-men-3_375x375_crop_center.jpg?v=1680691085%20x1",
+                "categorie": "Earrings"
             }
         ]
 
